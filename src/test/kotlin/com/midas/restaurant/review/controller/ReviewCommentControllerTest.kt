7c5529b505ea9fd3daf0b7163a262d7b96a7f5a6
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-06-23 12:05:48 +0900

    [test] 댓글, 대댓글 작성 테스트 코드 구현
    댓글, 대댓글 API 테스트 구현

--- src/test/kotlin/com/midas/restaurant/review/controller/ReviewCommentControllerTest.kt
+++ src/test/kotlin/com/midas/restaurant/review/controller/ReviewCommentControllerTest.kt
@@ -5,6 +5,8 @@ import com.midas.restaurant.common.contant.ResultStatus
 import com.midas.restaurant.config.TestSecurityConfig
 import com.midas.restaurant.exception.CustomException
 import com.midas.restaurant.member.dto.MemberDetails
+import com.midas.restaurant.review.dto.CommentDto
+import com.midas.restaurant.review.dto.request.CommentRequest
 import com.midas.restaurant.review.service.CommentService
 import com.ninjasquad.springmockk.MockkBean
 import io.kotest.assertions.print.print
@@ -22,6 +24,7 @@ import org.springframework.data.domain.Pageable
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(controllers = [ReviewCommentController::class])
@@ -101,6 +104,97 @@ class ReviewCommentControllerTest(
                             any(Pageable::class)
                         )
                     }
+                }
+            }
+        }
+
+        describe("리뷰 댓글 작성") {
+            val reviewId = 1L
+            val request = CommentRequest(content = "test")
+            context("댓글 작성 성공") {
+                every { commentService.postComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                    .returns(CommentDto(id = 1L, content = request.content!!))
+                it("200 OK") {
+                    mvc.perform(post("/api/v1/review/$reviewId/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                        .andExpect(status().isOk)
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("code").value(ResultStatus.SUCCESS.code))
+                        .andExpect(jsonPath("message").value(ResultStatus.SUCCESS.message))
+                        .andExpect(jsonPath("data.content").value(request.content))
+                    verify { commentService.postComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                }
+            }
+            context("댓글 작성 실패") {
+                every { commentService.postComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                    .throws(CustomException(ResultStatus.ACCESS_NOT_EXIST_ENTITY))
+                it("4XX ERROR") {
+                    mvc.perform(post("/api/v1/review/$reviewId/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                        .andExpect(status().isBadRequest)
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("code").value(ResultStatus.ACCESS_NOT_EXIST_ENTITY.code))
+                        .andExpect(jsonPath("message").value(ResultStatus.ACCESS_NOT_EXIST_ENTITY.message))
+                    verify { commentService.postComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                }
+            }
+            context("일부 파라미터 누락 시") {
+                it("400 ERROR") {
+                    mvc.perform(post("/api/v1/review/$reviewId/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(CommentRequest(content = null))))
+                        .andExpect(status().isBadRequest)
+                        .andExpect(jsonPath("code").value(ResultStatus.NOT_VALID_REQUEST.code))
+                        .andExpect(jsonPath("message").exists())
+                        .andExpect(jsonPath("data").isArray)
+                }
+            }
+        }
+
+        describe("대댓글 작성") {
+            val reviewId = 1L
+            val commentId = 1L
+            val request = CommentRequest(content = "test")
+            context("대댓글 작성 성공") {
+                every { commentService.postChildComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                    .returns(CommentDto(id = 1L, content = request.content!!))
+                it("200 OK") {
+                    mvc.perform(post("/api/v1/review/$reviewId/comment/$commentId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                        .andExpect(status().isOk)
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("code").value(ResultStatus.SUCCESS.code))
+                        .andExpect(jsonPath("message").value(ResultStatus.SUCCESS.message))
+                        .andExpect(jsonPath("data.content").value(request.content))
+                    verify { commentService.postChildComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                }
+            }
+            context("대댓글 작성 실패") {
+                every { commentService.postChildComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                    .throws(CustomException(ResultStatus.ACCESS_NOT_EXIST_ENTITY))
+                it("4XX ERROR") {
+                    mvc.perform(post("/api/v1/review/$reviewId/comment/$commentId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                        .andExpect(status().isBadRequest)
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("code").value(ResultStatus.ACCESS_NOT_EXIST_ENTITY.code))
+                        .andExpect(jsonPath("message").value(ResultStatus.ACCESS_NOT_EXIST_ENTITY.message))
+                    verify { commentService.postChildComment(any(Long::class), any(Long::class), any(CommentDto::class)) }
+                }
+            }
+            context("일부 파라미터 누락 시") {
+                it("400 ERROR") {
+                    mvc.perform(post("/api/v1/review/$reviewId/comment/$commentId")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(CommentRequest(content = null))))
+                        .andExpect(status().isBadRequest)
+                        .andExpect(jsonPath("code").value(ResultStatus.NOT_VALID_REQUEST.code))
+                        .andExpect(jsonPath("message").exists())
+                        .andExpect(jsonPath("data").isArray)
                 }
             }
         }
