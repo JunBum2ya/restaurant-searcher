3c49a782fe9f99da09e891281d8431b36c7a4e4b
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-07-01 17:47:00 +0900

    [feat] cors 설정 추가
    cors 설정을 추가하여 front에서 조회가 가능하다록 한다.

--- src/main/kotlin/com/midas/restaurant/config/SecurityConfig.kt
+++ src/main/kotlin/com/midas/restaurant/config/SecurityConfig.kt
@@ -1,18 +1,22 @@
 package com.midas.restaurant.config
 
-import com.midas.restaurant.common.component.JwtTokenProvider
 import com.midas.restaurant.common.component.JwtAuthenticationEntryPoint
+import com.midas.restaurant.common.component.JwtTokenProvider
 import com.midas.restaurant.common.filter.JwtTokenFilter
 import com.midas.restaurant.exception.JwtAccessDeniedHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class SecurityConfig(
@@ -36,6 +40,17 @@ class SecurityConfig(
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .addFilterBefore(JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter::class.java)
             .build()
+    }
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
     }
 
     @Bean
